74eaab95ca10dd4f04491e5a85b39fc45abdc2f8
Rezevoo <arazavi@identecsolutions.com>
2024-02-27 17:49:16 +0100

    remove invoice menu

--- src/app/shared/services/navigation/navigation-configuration.ts
+++ src/app/shared/services/navigation/navigation-configuration.ts
@@ -3,5 +3,4 @@ import { NavigationItem } from "#shared/models/navigation-item.model";
 export const NAVIGATION_CONFIGURATION: NavigationItem[] = [
     { displayName: 'Home', icon: 'home', link: 'tasks-list' },
     { displayName: 'History', icon: 'history', link: 'history' },
-    { displayName: 'Invoice', icon: 'assignment', link: 'invoice' },
 ];
